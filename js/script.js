cedbd3432b8c601a2eab02f7f1ff814d0b67a562
POleskiewicz <poleskiewicz@hawk.iit.edu>
2021-04-20 22:42:01 -0500

    Added js source

--- js/script.js
+++ js/script.js
@@ -5,16 +5,8 @@ function initMap() {
 	
 	
 	
-	<div id="map"></div>
-	<script>
-		var map;
-		function initMap() {
-			map = new google.maps.Map(document.getElementById('map'), {
-				center: {lat: -34.397, lng: 150.644},
-				zoom: 8
-				});
-			}
-			</script>
+	
+	
 	
 	
 	
