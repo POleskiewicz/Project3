09da95bbe5ede966591224649e26b9e5c32a3736
POleskiewicz <poleskiewicz@hawk.iit.edu>
2021-04-20 23:14:36 -0500

    Added first js update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
-
-function initMap() {
-	alert('it works');
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
-
-google.maps.event.addDomListener(window, 'load', init);
+function initMap(){
+var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(51.9194, 19.1451);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 6,
+		mapTypeId: google.maps.MapTypeId.roadmap,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
